e18e08e26ed260b8da581b0c4df9e44c83dce23f
Olivier Goffart <ogoffart@woboq.com>
2016-03-12 12:53:06 +0100

    Change the default theme to qtcreator. It seems people like it more

--- data/codebrowser.js
+++ data/codebrowser.js
@@ -66,7 +66,7 @@ if (style_match) {
     var style = style_match[1];
     switchStylestyleAndSetCookie(style);
 } else {
-    var c = readCookie('style');
+    var c = readCookie('style') || "qtcreator";
     if (c) switchStylestyle(c);
 }
 
