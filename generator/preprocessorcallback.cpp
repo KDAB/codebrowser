5f0f13675a754034d6222e78fba4eff39f9eff78
Jean-Christophe Delaunay <52913047+Jissynacktiv@users.noreply.github.com>
2019-07-15 18:02:20 +0200

    Switch from fixed major version to all new coming ones
    Co-Authored-By: Olivier Goffart <olivier@woboq.com>

--- generator/preprocessorcallback.cpp
+++ generator/preprocessorcallback.cpp
@@ -105,7 +105,7 @@ void PreprocessorCallback::MacroExpands(const clang::Token& MacroNameTok,
     PP.setPragmasEnabled(false);
     seenPragma = false;
 
-#if CLANG_VERSION_MAJOR == 9
+#if CLANG_VERSION_MAJOR >= 9
     PP.EnterTokenStream(tokens, /*DisableMacroExpansion=*/false, /*IsReinject=*/false);
 #elif CLANG_VERSION_MAJOR != 3 || CLANG_VERSION_MINOR >= 9
     PP.EnterTokenStream(tokens, /*DisableMacroExpansion=*/false);
